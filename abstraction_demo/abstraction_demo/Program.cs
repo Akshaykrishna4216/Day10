23b5507e1995e46ed6f4968c4fe4a32863188347
Akshaykrishna4216 <129931476+Akshaykrishna4216@users.noreply.github.com>
2023-04-17 11:25:29 +0530

    Add files via upload

---
+++ abstraction_demo/abstraction_demo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace abstraction_demo
+{
+    abstract class Figure
+    {
+        public int dimension;
+        public abstract double Area();
+        public abstract double Perimeter();
+    }
+    class Square : Figure
+    {
+        public override double Area()
+        {
+            return dimension * dimension;
+        }
+        public override double Perimeter()
+        {
+            return 4 * dimension;
+        }
+        class Circle : Figure
+        {
+            public override double Area()
+            {
+                return Math.PI * (dimension * dimension);
+            }
+            public override double Perimeter()
+            {
+                return 2 * Math.PI * dimension;
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Figure f1 = new Square();
+                f1.dimension = 10;
+                Console.WriteLine(f1.Area());
+                Console.WriteLine(f1.Perimeter());
+                Figure f2 = new Circle();
+                f2.dimension = 20;
+                Console.WriteLine(f2.Area());
+                Console.WriteLine(f2.Perimeter());
+            }
+        }
+
+
+    }
+}
---
+++ indexer/indexer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indexerdemo
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            indexer i;
+            i = new indexer();
+            i[3] = 4.3f;
+            Console.WriteLine(i[3]);
+
+        }
+    }
+}
